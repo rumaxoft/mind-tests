a2358a247c7e85e547542c368f4651089df37f5b
Maxim Rudenko <maximu.rudenko@gmail.com>
2024-03-21 02:11:22 +0300

    feat: test component

---
+++ src/components/SpecialEdge.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import type { EdgeProps } from '@vue-flow/core';
+import { BezierEdge } from '@vue-flow/core';
+
+import { CustomData } from './edges'
+
+// props were passed from the slot using `v-bind="customEdgeProps"`
+const props = defineProps<EdgeProps<CustomData>>();
+
+console.log(props.data.hello) // 'world'
+</script>
+
+<script lang="ts">
+export default {
+  name: 'CustomEdge',
+};
+</script>
+
+<template>
+  <BezierEdge
+      :source-x="sourceX"
+      :source-y="sourceY"
+      :target-x="targetX"
+      :target-y="targetY"
+      :source-position="sourcePosition"
+      :target-position="targetPosition"
+  />
+</template>
---
+++ src/components/SpecialNode.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { NodeProps } from '@vue-flow/core'  
+import { Position } from '@vue-flow/core'
+
+// props were passed from the slot using `v-bind="customNodeProps"`
+const props = defineProps<NodeProps>()
+  
+console.log(props.data.hello) // 'world'
+</script>
+
+<template>
+  <div class="vue-flow__node-custom">
+    <Handle type="target" :position="Position.Top" />
+    <div>{{ label }}</div>
+    <Handle type="source" :position="Position.Bottom" />
+  </div>
+</template>
